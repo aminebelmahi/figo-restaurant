57c7f9192524027237808124628b4586a5ae61d7
Revatto <revattojs@gmail.com>
2020-06-12 12:57:46 +0100

    testing Tabs with JS

---
+++ main.js
@@ -0,0 +1,12 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabsContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabsContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        target.classList.add('active');
+    })
+})
