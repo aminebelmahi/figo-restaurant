191856776cccdf96e33472e2711270c70e390489
Revatto <revattojs@gmail.com>
2020-06-16 19:40:43 +0100

    Add Active class onclick to li tab and remove it

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const tabs = document.querySelectorAll('[data-tab-target]');
 const tabsContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget);
-        tabsContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
-        target.classList.add('active');
+  tab.addEventListener('click', (e) => {
 
+    const target = document.querySelector(tab.dataset.tabTarget);
+
+    // Loop through 'menu-content' and remove 'active' class
+    tabsContents.forEach(tabContent => {
+      tabContent.classList.remove('active');
     })
+    // Add 'active' class to 'menu-content'
+    target.classList.add('active');
+
+    //loop through 'li' items and remove 'active' class
+    tabs.forEach(tab => {
+      tab.classList.remove('active');
+    })
+
+    //add 'active' class to clicked 'li' item
+    e.target.classList.add("active");
+
+  })
 })
