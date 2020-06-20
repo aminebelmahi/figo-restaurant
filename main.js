146872520bf72ae685721d99944358027f436fc2
Revatto <revattojs@gmail.com>
2020-06-20 11:02:45 +0100

    Testimonials section done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 ///// Adding functionalities to the Tab-Menu Section /////
+
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabsContents = document.querySelectorAll('[data-tab-content]');
 
@@ -24,3 +25,68 @@ tabs.forEach(tab => {
 
   })
 })
+
+
+///// Inject Testimony to HTML /////
+
+// Global UI Variables
+const img = document.querySelector('.avatar');
+const personName = document.querySelector('.name');
+const info = document.querySelector('.info');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+const testimonials = [{
+    name: 'Eric Lemon',
+    info: 'Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet, congue mauris rhoncus aenean vel.',
+    avatar: 'https://i.ibb.co/d7S9dBL/avatar-1.jpg'
+  },
+  {
+    name: 'Malika Emroud',
+    info: 'Varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere, congue mauris rhoncus aenean vel.',
+    avatar: 'https://i.ibb.co/nbp1D8W/avatar-2.jpg'
+  },
+  {
+    name: 'Linda Clair',
+    info: 'Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus.',
+    avatar: 'https://i.ibb.co/fHXtpQC/avatar-3.jpg'
+  },
+  {
+    name: 'Semantha Moon',
+    info: 'Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis.',
+    avatar: 'https://i.ibb.co/Mp8t3XD/avatar-4.jpg'
+  }
+]
+// Starting Item 
+let currentItem = 0;
+
+// Load initial item (on refresh)
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson(currentItem);
+});
+
+// Display person based on item
+function showPerson(person) {
+  const item = testimonials[person];
+  img.src = item.avatar;
+  personName.textContent = item.name;
+  info.textContent = item.info;
+};
+
+// Next Person Testimony
+nextBtn.addEventListener('click', (e) => {
+  currentItem++
+  if (currentItem > testimonials.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// Previous Person Testimony
+prevBtn.addEventListener('click', (e) => {
+  currentItem--
+  if (currentItem < 0) {
+    currentItem = testimonials.length - 1;
+  }
+  showPerson(currentItem);
+});
